12d1a0bb359c47ff85c2875a6764342f4e40d7df
csanry <clarencesan@gmail.com>
2022-05-23 15:28:16 +0800

    add model_dispatcher

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,9 +1,12 @@
-# from sklearn - to be added 
+from src import config
+from src.helpers import load_model
 
-# models = {
-#     "random_forest": randomforest(
+base_path = config.MODEL_OUTPUT_PATH
 
-#     ), 
-#     "decision_tree"
-
-# } 
+models = {
+    "gbm": load_model(base_path / "gbm.pickle"),
+    "log_reg": load_model(base_path / "log_reg.pickle"),
+    "nca": load_model(base_path / "nca.pickle"),
+    "rf": load_model(base_path / "rf.pickle"),
+    "ada": load_model(base_path / "adaboost.pickle"),
+}
