6a8ed2e4d7cd450befa4fa9e098ad983cc23fd9c
csanry <clarencesan@gmail.com>
2022-06-01 12:14:49 +0800

    shell script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# read -p "Enter environment name: " -r env_name
-source /root/miniconda3/etc/profile.d/conda.sh 
-conda activate aml
-export PYTHONPATH="${PYTHONPATH}:/workspaces/aml"
-# conda activate aml
-python /workspaces/aml/src/data/make_dataset.py && python /workspaces/aml/src/features/build_features.py && python /workspaces/aml/src/models/train.py
+
+source activate aml
+
+pip install -e .
+
+python3 src/data/make_dataset.py &&\
+python3 src/features/build_features.py &&\
+python3 src/models/train.py
+
