ab199d3b4a92e80b9737f4b67b083f94697f6267
csanry <clarencesan@gmail.com>
2022-06-14 14:39:31 +0800

    added F05 reporting for all models

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,8 +4,16 @@ from typing import Type, Union
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import (accuracy_score, f1_score, fbeta_score,
-                             precision_score, recall_score, roc_auc_score, roc_curve, confusion_matrix)
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    f1_score,
+    fbeta_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+    roc_curve,
+)
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 
@@ -22,8 +30,8 @@ def evaluate_tuning(tuner: Union[Type[RandomizedSearchCV], Type[GridSearchCV]]) 
     TRAIN AUC SD: {tuner.cv_results_["std_train_AUC"][tuner.best_index_]:.2%}
     TEST AUC: {tuner.cv_results_["mean_test_AUC"][tuner.best_index_]:.2%}
     TEST AUC SD: {tuner.cv_results_["std_test_AUC"][tuner.best_index_]:.2%}
-    TRAIN F2: {tuner.cv_results_['mean_train_F2'][tuner.best_index_]:.2%}
-    TEST F2: {tuner.cv_results_['mean_test_F2'][tuner.best_index_]:.2%}  
+    TRAIN F_score: {tuner.cv_results_['mean_train_F_score'][tuner.best_index_]:.2%}
+    TEST F_score: {tuner.cv_results_['mean_test_F_score'][tuner.best_index_]:.2%}  
     """
     )
 
@@ -35,12 +43,13 @@ def evaluate_report(
     report["accuracy"] = accuracy_score(y_test, y_pred)
     report["precision"] = precision_score(y_test, y_pred)
     report["recall"] = recall_score(y_test, y_pred)
+    report["f05"] = fbeta_score(y_test, y_pred, beta=0.5)
     report["f1"] = f1_score(y_test, y_pred)
     report["f2"] = fbeta_score(y_test, y_pred, beta=2)
     report["cf_matrix"] = confusion_matrix(y_test, y_pred, normalize="all")
     report["auroc"] = roc_auc_score(y_test, y_pred_prob)
     report["roc"] = roc_curve(y_test, y_pred_prob)
-    
+
     print(
         f"""
     -----------
@@ -49,12 +58,14 @@ def evaluate_report(
     ACCURACY: {report["accuracy"]:.2%}
     PRECISION: {report["precision"]:.2%}
     RECALL: {report["recall"]:.2%}
+    F05: {report["f05"]:.2%}
     F1: {report["f1"]:.2%}
     F2: {report["f2"]:.2%}
     ROC AUC: {report["auroc"]:.2%}
     """
     )
     return report
+
 
 def main() -> None:
     pass
