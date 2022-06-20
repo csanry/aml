b9563bfcc95a88b176b8723af113a69058247b9e
csanry <clarencesan@gmail.com>
2022-06-20 21:56:45 +0800

    fix small bug
    fix bug

--- src/predict/predict_models.py
+++ src/predict/predict_models.py
@@ -12,7 +12,7 @@ def predict_all(small_loans_model: str = "rf", large_loans_model: str = "gbm"):
 
     logger = logging.getLogger()
 
-    large_loans, small_loans = helpers.read_pred_files()
+    large_loans_data, small_loans_data = helpers.read_pred_files()
 
     large_model = src.model_dispatcher.large_models.get(large_loans_model)
     small_model = src.model_dispatcher.small_models.get(small_loans_model)
@@ -20,15 +20,15 @@ def predict_all(small_loans_model: str = "rf", large_loans_model: str = "gbm"):
     for name, model, data in zip(
         [f"large_loans_{large_loans_model}", f"small_loans_{small_loans_model}"],
         [large_model, small_model],
-        [large_loans, small_loans],
+        [large_loans_data, small_loans_data],
     ):
         logger.info(f"PREDICTING {name}")
 
-        predictions = pd.Series(model.predict(data))
-        data = pd.concat([data, predictions], axis=1)
+        predictions = pd.Series(model.predict(data), name="prediction")
+        final = pd.concat([data, predictions], axis=1)
 
         logger.info(f"DONE {name}")
-        data.to_csv(config.FIN_FILE_PATH / f"{name}_prediction.parquet")
+        final.to_parquet(config.FIN_FILE_PATH / f"{name}_prediction.parquet")
 
 
 if __name__ == "__main__":
