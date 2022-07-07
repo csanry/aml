dd77f5cc2867ef501127c5ac87131704cde53836
csanry <clarencesan@gmail.com>
2022-07-08 01:03:00 +0800

    update predict models

--- src/predict/predict_models.py
+++ src/predict/predict_models.py
@@ -1,21 +1,20 @@
 import logging
 
 import pandas as pd
-import src.model_dispatcher
 from src import config, helpers
+from src.model_dispatcher import large_models, small_models
 
 
-def predict_all(small_loans_model: str = "rf", large_loans_model: str = "gbm"):
-
-    log_fmt = "%(asctime)s:%(name)s:%(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def predict_all(
+    small_loans_model: str = "rf_300000", large_loans_model: str = "gbm_300000"
+):
 
     logger = logging.getLogger()
 
     large_loans_data, small_loans_data = helpers.read_pred_files()
 
-    large_model = src.model_dispatcher.large_models.get(large_loans_model)
-    small_model = src.model_dispatcher.small_models.get(small_loans_model)
+    large_model = large_models.get(large_loans_model)
+    small_model = small_models.get(small_loans_model)
 
     for name, model, data in zip(
         [f"large_loans_{large_loans_model}", f"small_loans_{small_loans_model}"],
@@ -27,10 +26,18 @@ def predict_all(small_loans_model: str = "rf", large_loans_model: str = "gbm"):
         predictions = pd.Series(model.predict(data), name="prediction")
         final = pd.concat([data, predictions], axis=1)
 
-        logger.info(f"DONE {name}")
         final.to_parquet(config.FIN_FILE_PATH / f"{name}_prediction.parquet")
+        logger.info(
+            f"DONE {name}; PREDICTIONS AT: {config.FIN_FILE_PATH / f'{name}_prediction.parquet'}"
+        )
+
+    logger.info(f"DONE")
 
 
 if __name__ == "__main__":
-    predict_all()
+
+    log_fmt = "%(asctime)s:%(name)s:%(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    predict_all(small_loans_model="rf_300000", large_loans_model="gbm_300000")
 
